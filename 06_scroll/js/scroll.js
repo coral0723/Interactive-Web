892d215485abe0a60c5e277976daab9e57d55001
san9901 <san990190@gmail.com>
2025-07-01 23:33:51 +0900

    스크롤 값을 이용한 페럴렉스 페이지

---
+++ 06_scroll/js/scroll.js
@@ -0,0 +1,23 @@
+const depthWrap = document.querySelector(".depthWrap");
+const progressBar = document.querySelector(".bar");
+const submarine = document.querySelector(".submarine");
+const octopus = document.querySelector(".octopus");
+let scrollNum = 0;
+let documentHeight = 0;
+let per = 0;
+
+window.addEventListener("scroll", () => {
+  scrollNum = window.scrollY;
+  documentHeight = document.body.scrollHeight - window.innerHeight;
+  per = percent(scrollNum, documentHeight);
+
+  depthWrap.querySelector("span").innerText = per;
+  progressBar.style.width = per + "%";
+
+  submarine.style.transform = `translateX(${per}%)`;
+  octopus.style.transform = `translateY(${-per / 10}%)`;
+});
+
+const percent = (num, totalNum) => {
+  return ((num / totalNum) * 100).toFixed(0);
+}
